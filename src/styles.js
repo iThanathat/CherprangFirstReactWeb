56a7748ab0d4146657cc89e70e7ac0c2dfc3e46c
iThanathat <brain12456@hotmail.com>
2018-04-10 17:19:55 +0700

    Added my project

---
+++ src/styles.js
@@ -0,0 +1,13 @@
+const fontFamily = 'WDB Bangna';
+const background = 'pink url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQB9blZurrvnZ9GTkwVy-fTXPFYdUcNYHQb3R4mFX3HNHR3iCxcmA")fixed';
+const fontSize = '25px';
+const padding = '45px 0';
+const color = '#FF3333';
+export const styles = {
+  fontFamily: fontFamily,
+  backgroundImage: background,
+  fontSize: fontSize,
+  padding: padding,
+  color: color,
+  height: '100%'
+};
